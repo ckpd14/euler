b42ac91972038871ab80e80fd362dbd2457b4db0
fkagami <kagami1990@gmail.com>
2015-08-04 12:44:28 +0900

    Submit 001-kazy

---
+++ 001/kazy/01_stream.ex
@@ -0,0 +1,14 @@
+# https://projecteuler.net/problem=1
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+defmodule Problem1 do
+  def solve do
+    IO.puts 1..999 |> 
+    Enum.filter(fn(x) -> (rem(x, 3) == 0 || rem(x, 5) == 0) end) |>  
+    Enum.reduce(0, &(&1 + &2))
+  end
+end
+
+Problem1.solve
