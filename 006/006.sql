d27f1ffb7e60f34824b5b11941fc779130a996ad
Hiroyuki Inoue <mammymax@gmail.com>
2015-08-13 17:42:15 +0900

    Remove outer projection

--- 006/006.sql
+++ 006/006.sql
@@ -1,11 +1,6 @@
 select
-    a.exp_of_sum - a.sum_of_exp
+     (sum(i) ^ 2) - sum(i^2)
 from (
-    select
-        sum(i^2) as sum_of_exp
-        , sum(i) ^ 2 as exp_of_sum
-    from (
-        select * from generate_series(1, 100)
-    ) s(i)
-) a
+    select * from generate_series(1, 100)
+) s(i)
 ;
